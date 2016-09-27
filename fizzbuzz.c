35d4c6f7387021872c0202966749cf35d95645f4
stef <stef.verve@gmail.com>
2016-09-27 15:48:27 -0400

    First Commit

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
+// FizzBuzz, with added code for output readability
+
 int main() {
 	
 	for (int i = 1; i < 101; i++) {
-		
+		printf (" ");			// space preceeding each output
 		if (i % 3 != 0 && i % 5 != 0) {
-			printf ("%i", i);
+			printf ("%i  ", i);
+			if (i < 10) {
+				printf (" ");	// adds correct spacing for single digit numbers
+			}
+		} else {
+			if (i % 3 == 0) {
+				printf ("Fizz");
+			}
+			if (i % 5 == 0) {
+				printf ("Buzz\n");
+			}
 		}
-		if (i % 3 == 0) {
-			printf ("Fizz");
-		}
-		if (i % 5 == 0) {
-			printf ("Buzz");
-		}	
-		printf(" ");			// space for readability
 	}
 }
